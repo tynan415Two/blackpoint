c820529fa5e99db29f976c7970a6d9fbd7c71983
tynan415 <tynan_philip@me.com>
2022-03-25 00:17:53 -0700

    react/ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'eval-source-map',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
     plugins: [new HtmlWebpackPlugin({
         template: './src/index.html',
         filename: 'index.html'
