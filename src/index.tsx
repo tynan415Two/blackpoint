975e93c3221b0d62a41dc78dddabba8c6cd55b22
tynan415 <tynan_philip@me.com>
2022-03-25 16:49:36 -0700

    gh pages error two

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Nav from "./components/nav"
-import About from "./components/about";
-import "./styles/main.css";
-
-const App = () => <div><Nav /><About /></div>;
-
-ReactDOM.render(<App />, document.getElementById("root"))
