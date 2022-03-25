a13c93217f8905c29019d49afae095c639d12b93
tynan415 <tynan_philip@me.com>
2022-03-25 01:01:05 -0700

    css/plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -8,13 +9,27 @@ module.exports = {
             test: /\.tsx?$/,
             loader: 'babel-loader',
             exclude: /node_modules/
-        }]
+        }, 
+        {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader, 
+                {
+                    loader: 'css-loader',
+                    options: { modules: true }
+                }
+            ]
+        }
+    ]
     },
     resolve: {
         extensions: ['.js', '.ts', '.tsx']
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html'
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }), 
+        new MiniCssExtractPlugin(),
+    ]
 }
