81c721c390a613e157a8efe7203be26f17ea6dcb
tynan415 <tynan_philip@me.com>
2022-03-25 17:53:55 -0700

    size fix

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {}
+
+class Footer extends React.Component {
+    constructor(props: Props) {
+        super(props)
+    }
+    render() {
+        return <footer className="footer"></footer>
+    }
+}
+
+export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./components/nav"
 import About from "./components/about";
+import Footer from "./components/footer";
 import "./styles/main.css";
 
-const App = () => <div><Nav /><About /></div>;
+
+const App = () => <div><Nav /><About /><Footer/></div>;
 
 ReactDOM.render(<App />, document.getElementById("root"))
