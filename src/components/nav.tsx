9af13ed11cede824ea7599124cddc023601ae767
tynan415 <tynan_philip@me.com>
2022-03-27 13:35:48 -0700

    swipe

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,69 +1,74 @@
 import React, { MouseEvent } from "react";
 import NavImage from '../images/Black Point_SF Peninsula_Coast Survey_1869.png';
+interface Props {
+    selected: string
+}
 class Nav extends React.Component {
-   
-    render() {
-        const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-            e.preventDefault();
-            const button: HTMLButtonElement = e.currentTarget;
-            let name = button.textContent?.toLowerCase().replace('s', '');
-           if (name) {
-                let ele = document.getElementById(name);
-                let selChange = document.getElementById(name + "Swipe");
-                if (ele !== null && selChange !== null) {
-                    ele.style.display = "block";
-                    selChange.style.color = "white"
-                    if (name === "about") {
-                        let off1 = document.getElementById("photo")
-                        let colChange1 = document.getElementById("photoSwipe")
-                        if (off1 !== null && colChange1 !== null) {
-                            off1.style.display = "none";
-                            colChange1.style.color = "darkgray";
-                        }
-                        let off2 = document.getElementById("info")
-                        let colChange2 = document.getElementById("infoSwipe")
-                        if (off2 !== null && colChange2 !== null) {
-                            off2.style.display = "none";
-                            colChange2.style.color = "darkgray";
-                        }
-                    } else if (name === "photo") {
-                        let off1 = document.getElementById("about")
-                        let colChange1 = document.getElementById("aboutSwipe")
-                        if (off1 !== null && colChange1 !== null) {
-                            off1.style.display = "none";
-                            colChange1.style.color = "darkgray";
-                        }
-                        let off2 = document.getElementById("info")
-                        let colChange2 = document.getElementById("infoSwipe")
-                        if (off2 !== null && colChange2 !== null) {
-                            off2.style.display = "none";
-                            colChange2.style.color = "darkgray";
-                        }
-                    } else if(name === "info") {
-                        let off1 = document.getElementById("about")
-                        let colChange1 = document.getElementById("aboutSwipe")
-                        if (off1 !== null && colChange1 !== null) {
-                            off1.style.display = "none";
-                            colChange1.style.color = "darkgray";
-                        }
-                        let off2 = document.getElementById("photo")
-                        let colChange2 = document.getElementById("photoSwipe")
-                        if (off2 !== null && colChange2 !== null) {
-                            off2.style.display = "none";
-                            colChange2.style.color = "darkgray";
-                        }
+    constructor(props: Props) {
+        super(props)
+    }
+    handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        const button: HTMLButtonElement = e.currentTarget;
+        let name = button.textContent?.toLowerCase().replace('s', '');
+       if (name) {
+            let ele = document.getElementById(name);
+            let selChange = document.getElementById(name + "Swipe");
+            if (ele !== null && selChange !== null) {
+                ele.style.display = "block";
+                selChange.style.color = "white"
+                if (name === "about") {
+                    let off1 = document.getElementById("photo")
+                    let colChange1 = document.getElementById("photoSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if (name === "photo") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if(name === "info") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("photo")
+                    let colChange2 = document.getElementById("photoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
                     }
                 }
             }
         }
+    }
+    render() {
         return( 
         <nav className="nav">
             <img src={NavImage} className="headerImg" ></img>
             <h1 className="title">Blackpoint Cigar Club</h1>
             <div className="btnContainer">
-                <button className="aboutSwipe" id="aboutSwipe" onClick={handleClick}>About</button>
-                <button className="photoSwipe" id="photoSwipe" onClick={handleClick}>Photos</button>
-                <button className="infoSwipe" id="infoSwipe" onClick={handleClick}>Info</button>
+                <button className="aboutSwipe" id="aboutSwipe" onClick={this.handleClick}>About</button>
+                <button className="photoSwipe" id="photoSwipe" onClick={this.handleClick}>Photos</button>
+                <button className="infoSwipe" id="infoSwipe" onClick={this.handleClick}>Info</button>
             </div>
         </nav>
         )
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,88 @@ import Photos  from "./components/photos";
 import Info from "./components/info";
 import "./styles/main.css";
 
+let touchstartX = 0
+let touchendX = 0
 
-const App = () => <div><Nav /><About /><Photos /><Footer /><Info /></div>;
+const slider = document.getElementById('root')
+let selectedArr = ["about", "photo", "info"];
+let idx = 0;
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        idx < selectedArr.length -1 ? idx++ : idx;
+    }
+    if (touchendX > touchstartX) {
+        idx > 0 ? idx-- : idx;
+    }
+    let name = selectedArr[idx];
+    let ele = document.getElementById(name)
+    let selChange = document.getElementById(name + "Swipe");
+            if (ele !== null && selChange !== null) {
+                ele.style.display = "block";
+                selChange.style.color = "white"
+                if (name === "about") {
+                    let off1 = document.getElementById("photo")
+                    let colChange1 = document.getElementById("photoSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if (name === "photo") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if(name === "info") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("photo")
+                    let colChange2 = document.getElementById("photoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                }
+            }
+    
+}
+
+slider?.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+slider?.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
+
+const App = () => {
+    return(
+        <div>
+            <Nav  />
+            <About />
+            <Photos />
+            <Info />
+            <Footer />
+        </div>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById("root"))
