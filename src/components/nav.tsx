8ad96ee3c53b377d42fe24002b33c46b5b72d2e6
tynan415 <tynan_philip@me.com>
2022-03-26 22:50:44 -0700

    event handler start

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,22 +1,41 @@
 import React from "react";
-import NavImage from '../images/Black Point_SF Peninsula_Coast Survey_1869.png'
-interface Props {}
-interface State {
-    
-}
+import NavImage from '../images/Black Point_SF Peninsula_Coast Survey_1869.png';
+
+interface Props {};
+// type selected = "About" | "Photos" | "Info";
+
 class Nav extends React.Component {
     constructor(props: Props) {
         super(props)
+        this.state = {
+            selected: "About",
+        }
     }
+
+    handleClick(e: { preventDefault: () => void; target: { textContent: any; }; }) {
+        e.preventDefault();
+        let name = e.target.textContent;
+        console.log(name)
+
+        if (name === "About") {
+
+        } else if (name === "Photos") {
+
+        } else if(name === "Info") {
+
+        }
+
+    }
+
     render() {
         return( 
         <nav className="nav">
             <img src={NavImage} className="headerImg" ></img>
             <p className="title">Blackpoint Cigar Club</p>
             <div className="btnContainer">
-                <button className="aboutSwipe">About</button>
-                <button className="photoSwipe">Photos</button>
-                <button className="infoSwipe">Info</button>
+                <button className="aboutSwipe" onClick={this.handleClick}>About</button>
+                <button className="photoSwipe" onClick={this.handleClick}>Photos</button>
+                <button className="infoSwipe" onClick={this.handleClick}>Info</button>
             </div>
         </nav>
         )
