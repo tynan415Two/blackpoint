f1e4b6a5f82d1bdcdb93a6ea7496f0b7dbcf93db
tynan415 <tynan_philip@me.com>
2022-03-27 20:07:00 -0700

    fix

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,97 @@
 import React, { MouseEvent } from "react";
 import NavImage from '../images/Black Point_SF Peninsula_Coast Survey_1869.png';
-interface Props {
-    selected: string
-}
-class Nav extends React.Component {
-    constructor(props: Props) {
-        super(props)
+import photosArr from "./utils/photosUtil";
+
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const slider = document.getElementById('root')
+let selectedArr = ["about", "photo", "info"];
+let idx = 0;
+function handleGesture() {
+    console.log("handle", idx)
+    if (touchendX < touchstartX + 15 && Math.abs(touchstartY - touchendY) < 15) {
+        idx < selectedArr.length -1 ? idx++ : idx;
     }
+    if (touchendX > touchstartX -15 && Math.abs(touchstartY - touchendY) < 15) {
+        idx > 0 ? idx-- : idx;
+
+    }
+    let name = selectedArr[idx];
+    let ele = document.getElementById(name)
+    let selChange = document.getElementById(name + "Swipe");
+            if (ele !== null && selChange !== null) {
+                ele.style.display = "block";
+                selChange.style.color = "white"
+                if (name === "about") {
+                    let off1 = document.getElementById("photo")
+                    let colChange1 = document.getElementById("photoSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if (name === "photo") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("info")
+                    let colChange2 = document.getElementById("infoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                } else if(name === "info") {
+                    let off1 = document.getElementById("about")
+                    let colChange1 = document.getElementById("aboutSwipe")
+                    if (off1 !== null && colChange1 !== null) {
+                        off1.style.display = "none";
+                        colChange1.style.color = "darkgray";
+                    }
+                    let off2 = document.getElementById("photo")
+                    let colChange2 = document.getElementById("photoSwipe")
+                    if (off2 !== null && colChange2 !== null) {
+                        off2.style.display = "none";
+                        colChange2.style.color = "darkgray";
+                    }
+                }
+            }
+    
+}
+
+slider?.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+  touchstartY = e.changedTouches[0].screenY
+})
+
+slider?.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  touchendY = e.changedTouches[0].screenY
+  console.log(touchstartX, touchendX)
+  if (touchstartX === touchendX || Math.abs(touchstartX - touchendX) < 25) return
+  handleGesture()
+})
+class Nav extends React.Component {
+  
     handleClick = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         const button: HTMLButtonElement = e.currentTarget;
         let name = button.textContent?.toLowerCase().replace('s', '');
+
        if (name) {
+            idx = selectedArr.indexOf(name);
+            console.log(selectedArr.indexOf(name))
+
             let ele = document.getElementById(name);
             let selChange = document.getElementById(name + "Swipe");
             if (ele !== null && selChange !== null) {
@@ -61,16 +141,42 @@ class Nav extends React.Component {
         }
     }
     render() {
-        return( 
-        <nav className="nav">
-            <img src={NavImage} className="headerImg" ></img>
-            <h1 className="title">Blackpoint Cigar Club</h1>
-            <div className="btnContainer">
-                <button className="aboutSwipe" id="aboutSwipe" onClick={this.handleClick}>About</button>
-                <button className="photoSwipe" id="photoSwipe" onClick={this.handleClick}>Photos</button>
-                <button className="infoSwipe" id="infoSwipe" onClick={this.handleClick}>Info</button>
-            </div>
-        </nav>
+        let photos = photosArr;
+        return(
+        <div>
+            <nav className="nav">
+                <img src={NavImage} className="headerImg" ></img>
+                <h1 className="title">Blackpoint Cigar Club</h1>
+                <div className="btnContainer">
+                    <button className="aboutSwipe" id="aboutSwipe" onClick={this.handleClick}>About</button>
+                    <button className="photoSwipe" id="photoSwipe" onClick={this.handleClick}>Photos</button>
+                    <button className="infoSwipe" id="infoSwipe" onClick={this.handleClick}>Info</button>
+                </div>
+            </nav>
+            <section className="about" id="about">
+                <article>
+                Located across the street from Aquatic Park with breathtaking views of the San Francisco Bay,  
+                Black Point Cigar Club operates as an exclusive private cigar club and special event space. 
+                The club is named after Black Point, the northernmost peak on the San Francisco mainland. 
+                Black Point is the last remaining section of original coastline in the city east of the Golden Gate Bridge.
+                </article>
+            </section>
+
+            <ul className="photos" id="photo">
+                {photos.map((photo: string, i: number) => <li key={i}><img className="photo" src={photo}></img></li>)}
+            </ul>
+
+            <section className="info" id="info">
+                <h2>Rentals</h2>
+                <p>Black Point Cigar Club is available for rentals starting at $150/hour, 4 hours minimum. Seasonal catering is available for an additional fee.</p>
+                <h2>Membership</h2>
+                <p>Private memberships are available for $300/month.</p>
+                <h2>Contact</h2>
+                <p>blackpointcigarclub@gmail.com</p>
+            </section>
+
+            <footer className="footer"></footer>
+        </ div>
         )
         
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,99 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./components/nav"
-import About from "./components/about";
-import Footer from "./components/footer";
-import Photos  from "./components/photos";
-import Info from "./components/info";
 import "./styles/main.css";
 
-let touchstartX = 0;
-let touchstartY = 0;
-let touchendX = 0;
-let touchendY = 0;
 
-const slider = document.getElementById('root')
-let selectedArr = ["about", "photo", "info"];
-let idx = 0;
-function handleGesture() {
-    if (touchendX < touchstartX + 15 && Math.abs(touchstartY - touchendY) < 15) {
-        idx < selectedArr.length -1 ? idx++ : idx;
-    }
-    if (touchendX > touchstartX -15 && Math.abs(touchstartY - touchendY) < 15) {
-        idx > 0 ? idx-- : idx;
-
-    }
-    let name = selectedArr[idx];
-    let ele = document.getElementById(name)
-    let selChange = document.getElementById(name + "Swipe");
-            if (ele !== null && selChange !== null) {
-                ele.style.display = "block";
-                selChange.style.color = "white"
-                if (name === "about") {
-                    let off1 = document.getElementById("photo")
-                    let colChange1 = document.getElementById("photoSwipe")
-                    if (off1 !== null && colChange1 !== null) {
-                        off1.style.display = "none";
-                        colChange1.style.color = "darkgray";
-                    }
-                    let off2 = document.getElementById("info")
-                    let colChange2 = document.getElementById("infoSwipe")
-                    if (off2 !== null && colChange2 !== null) {
-                        off2.style.display = "none";
-                        colChange2.style.color = "darkgray";
-                    }
-                } else if (name === "photo") {
-                    let off1 = document.getElementById("about")
-                    let colChange1 = document.getElementById("aboutSwipe")
-                    if (off1 !== null && colChange1 !== null) {
-                        off1.style.display = "none";
-                        colChange1.style.color = "darkgray";
-                    }
-                    let off2 = document.getElementById("info")
-                    let colChange2 = document.getElementById("infoSwipe")
-                    if (off2 !== null && colChange2 !== null) {
-                        off2.style.display = "none";
-                        colChange2.style.color = "darkgray";
-                    }
-                } else if(name === "info") {
-                    let off1 = document.getElementById("about")
-                    let colChange1 = document.getElementById("aboutSwipe")
-                    if (off1 !== null && colChange1 !== null) {
-                        off1.style.display = "none";
-                        colChange1.style.color = "darkgray";
-                    }
-                    let off2 = document.getElementById("photo")
-                    let colChange2 = document.getElementById("photoSwipe")
-                    if (off2 !== null && colChange2 !== null) {
-                        off2.style.display = "none";
-                        colChange2.style.color = "darkgray";
-                    }
-                }
-            }
-    
-}
-
-slider?.addEventListener('touchstart', e => {
-  touchstartX = e.changedTouches[0].screenX
-  touchstartY = e.changedTouches[0].screenY
-})
-
-slider?.addEventListener('touchend', e => {
-  touchendX = e.changedTouches[0].screenX
-  touchendY = e.changedTouches[0].screenY
-  handleGesture()
-})
 
 const App = () => {
-    return(
-        <div>
-            <Nav />
-            <About />
-            <Photos />
-            <Info />
-            <Footer />
-        </div>
-    )
+    return <Nav />
 };
 
 ReactDOM.render(<App />, document.getElementById("root"))
